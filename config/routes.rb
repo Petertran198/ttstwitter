ad3709e88dbd55b8485d7afaf371203921a1cee2
petertran198 <petertran198@gmail.com>
2018-12-31 17:58:26 -0500

    Implemented devise gem , tweet controller for seeing who is following , and intial design for twitter

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'feed' => 'epicenter#feed'
+  get 'show_user' => 'epicenter#show_user'
+  get 'now_following' => 'epicenter#now_following'
+  get 'unfollow' => 'epicenter#unfollow'
+  resources :tweets
+  devise_for :users
+  root "epicenter#feed"
+end
